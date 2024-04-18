e338ebf4f4a63f2d6b307d49fcf2a33cca9d4103
rajnish <rajnish.singh@baxture.com>
2024-04-18 19:51:37 +0530

    fixed css

--- app/Component/Card/page.tsx
+++ app/Component/Card/page.tsx
@@ -2,6 +2,7 @@ import { Anchor, Button, Flex, Paper, Text } from "@mantine/core";
 import { IconTrash, IconUserMinus, IconUserPlus } from "@tabler/icons-react";
 import UserImage from "../Logo/page";
 import { useState } from "react";
+import { IconWorld, IconPhoneCall } from "@tabler/icons-react";
 
 interface CardProps {
   id: number;
@@ -93,21 +94,8 @@ const Card: React.FC<CardProps> = ({
             size={"md"}
             onClick={(e) => e.preventDefault()}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-phone"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-            </svg>
+            <IconPhoneCall size={18} />
+
             {phone}
           </Anchor>
           <Anchor
@@ -118,25 +106,8 @@ const Card: React.FC<CardProps> = ({
             size={"md"}
             onClick={(e) => e.preventDefault()}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-world"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
-              <path d="M3.6 9h16.8" />
-              <path d="M3.6 15h16.8" />
-              <path d="M11.5 3a17 17 0 0 0 0 18" />
-              <path d="M12.5 3a17 17 0 0 1 0 18" />
-            </svg>{" "}
+            <IconWorld size={18} />
+
             {website}
           </Anchor>
         </Flex>
@@ -144,6 +115,7 @@ const Card: React.FC<CardProps> = ({
           <Button
             onClick={() => handleFollowUser(id)}
             fullWidth
+            variant={followUser ? "default" : "filled"}
             mr="5px"
             leftSection={
               followUser ? (
@@ -153,7 +125,7 @@ const Card: React.FC<CardProps> = ({
               )
             }
           >
-            Follow
+            {followUser ? "Unfollow" : "Follow"}
           </Button>
           <Button
             fullWidth
